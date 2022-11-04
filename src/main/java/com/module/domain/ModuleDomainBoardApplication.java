980fc5b06c33953283026bdc48fc593348f6749a
qwer9052 <qwer9052@gmail.com>
2022-11-04 21:21:04 +0900

    [Add] 프로젝트 첫 커밋

---
+++ src/main/java/com/module/domain/ModuleDomainBoardApplication.java
@@ -0,0 +1,13 @@
+package com.module.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleDomainBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleDomainBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/module/domain/ModuleDomainBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.module.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleDomainBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
